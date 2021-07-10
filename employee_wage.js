8cfd137bf991b5dde1f7b24301e0ee72ec0e69d3
anupama cv <anu.anupamacv@gmail.com>
2021-07-10 15:50:05 +0530

    Added:functionality to calculate total wages using arrow functions

--- employee_wage.js
+++ employee_wage.js
@@ -173,3 +173,23 @@ return totalWage + dailyWage;
 }
 console.log("uc 7A - Emp wage map total hrs: " + Array.from(empDaiilyWageMap.values()).reduce(totalWages, 0));
 
+//uc9
+let empDailyHrsMap = new Map();
+const findTotal = (totalVal, dailyVal) => {
+return totalVal + dailyVal;
+}
+let count = 0;
+let totalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal, 0);
+let totalSalary = empDailyWageArr.filter(dailyWage => dailyWage > 0).reduce(findTotal,0);
+console.log("uc9 - Emp wage with Arrow.:" + " ,Total hours: " + totalHours + "Total wage: " + totalSalary);
+let nonWorkingDays = new Array();
+let partWorkingdays = new Array();
+let fullWorkingDays = new Array();
+empDailyHrsMap.forEach( (value, key, map) => {
+if(value == 8) fullWorkingDays.push(key);
+else if(value ==4) partWorkingdays.push(key);
+else nonWorkingDays.push(key);
+});
+console.log("Full working days:" + fullWorkingDays);
+console.log("Part working days: " + partWorkingdays);
+console.log("Non working days: " + nonWorkingDays);
