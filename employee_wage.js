11e58e8a6d8c11905c4f88e8a57aaa3720d52274
anupama cv <anu.anupamacv@gmail.com>
2021-07-10 15:05:55 +0530

    Added: functionality to check employee present or not

---
+++ employee_wage.js
@@ -0,0 +1,9 @@
+//uc1
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == IS_ABSENT) {
+console.log("uc1 - Employee is absent");
+return;
+} else {
+console.log("uc1 - Employee is present");
+}
