0618eb82c218f6c7a3bb5c6a282093219b9f0ae3
anupama cv <anu.anupamacv@gmail.com>
2021-07-10 15:25:18 +0530

    Added: functionality to calculate daily wage along with total wage

--- employee_wage.js
+++ employee_wage.js
@@ -66,3 +66,26 @@ totalEmpHrs += getWorkingHours(empCheck);
 }
 empWage = totalEmpHrs * WAGE_PER_HOUR;
 console.log("uc5 - Total Days:" + totalWorkingDays + " ,Total Hrs: " + totalEmpHrs + " ,Emp Wage" + empWage);
+
+//uc6
+function calcDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+    }
+    
+    MAX_HRS_IN_MONTH = 160;
+    NUM_OF_WORKING_DAYS = 20;
+    totalEmpHrs = 0;
+    totalWorkingDays = 0;
+    let empDailyWageArr = new Array();
+    
+    while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
+    }
+    
+    empWage = calcDailyWage(totalEmpHrs);
+    console.log("uc6 - Total Days: " + totalWorkingDays + " ,Total Hrs: " + totalEmpHrs + " ,Emp wage: " + empWage);
+    
